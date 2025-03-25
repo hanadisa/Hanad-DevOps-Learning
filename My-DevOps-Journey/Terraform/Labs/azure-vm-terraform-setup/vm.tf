f8da298f6368cb0248abafa66d241fa7ac58eee0
Hanad Isa <hanadisa05@gmail.com>
2025-03-25 22:20:17 +0000

    Updated Terraform config and cleanup for Azure VM setup

--- My-DevOps-Journey/Terraform/Labs/azure-vm-terraform-setup/vm.tf
+++ My-DevOps-Journey/Terraform/Labs/azure-vm-terraform-setup/vm.tf
@@ -30,12 +30,13 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = "basic-vm"
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  size                  = "Standard_B1s"
-  admin_username        = "azureuser"
-  admin_password        = "P@ssword1234!"  # Only for testing
+  name                = "basic-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  admin_password      = "P@ssword1234!" # Only for testing
+  disable_password_authentication = false
 
   network_interface_ids = [
     azurerm_network_interface.nic.id,
